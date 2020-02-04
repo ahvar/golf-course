2da73b0b6913d309484c58a16ee7e22da526a022
Arthur Vargas <q1016330@vsd-css01.eacc.ds.quintiles.com>
2020-02-04 13:30:22 -0500

    some test code and minor changes to the golf_tour class

---
+++ tests_lesson02/project01a/project1a_starter_code/wake_golf_tour_a/test_golf_tour.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_create_golf_courses(self):
+        self.assertEqual(True, False)
+
+    def test_create_holes(self):
+        pass
+
+    def test_create_golfers(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests_lesson02/project01a/project1a_starter_code/wake_golf_tour_a/test_golfer.py
+++ tests_lesson02/project01a/project1a_starter_code/wake_golf_tour_a/test_golfer.py
@@ -1,9 +1,18 @@
 import unittest
-
+from golfer import Golfer
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_to_sql_date(self):
+        # positive test case
+        golfer_id = 1
+        golfer_name = 'Arthur Vargas'
+        golfer_bday = '04-14-83'
+        arthur = Golfer(golfer_id, golfer_name, golfer_bday)
+        self.assertEqual(str(arthur), '1,Arthur Vargas,04-14-1983')
+
+        # negative test case: a separator
+
+
 
 
 if __name__ == '__main__':
